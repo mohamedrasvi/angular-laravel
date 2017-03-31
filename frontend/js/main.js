cb85bfa033d858807b52a155e29e1ccec1de4ee8
Hermes Martins <hdmartins.ufabc@gmail.com>
2017-03-31 17:30:56 -0300

    Add laravel-cors no backend

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,1 +1,17 @@
+var app = angular.module('myApp', []);
+
+app.controller('usersController', function($scope, $http){
+
+	$scope.getAllUsers = function(){
+		$http.get('http://crud.dev/api/users').then(function(response){
+			console.log(response);
+		});
+		// $http.get('http://crud.dev/api/users').then(function(response){
+		// 		console.log("oi");
+		// 	});
+	};
+	
+
+});
+
 
